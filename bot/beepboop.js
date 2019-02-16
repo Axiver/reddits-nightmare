1b9f0b1bdcef6b93cd7d64687e03828c0c2e3bf2
Garlicvideos <Garlicvideos@gmail.com>
2019-02-16 18:01:19 +0800

    image upload
    working image upload

--- bot/beepboop.js
+++ bot/beepboop.js
@@ -1,18 +1,19 @@
 //Include login details
 var accdetails = require('./account.json');
 
+//Initialize instagram library
 var Client = require('instagram-private-api').V1;
 var device = new Client.Device(accdetails["username"]);
 var storage = new Client.CookieFileStorage('./cookies/gitgagged.json');
  
 //Login
 Client.Session.create(device, storage, accdetails["username"], accdetails["password"]).then(function(session) {
-	//Follow an account as a test
-	return [session, Client.Account.searchForUser(session, 'instagram')]   
-}).spread(function(session, account) {
-	return Client.Relationship.create(session, account.id);
-}).then(function(relationship) {
-	console.log(relationship.params)
-	// {followedBy: ... , following: ... }
-	// yayayay, you just followed @instagram
+	//Upload sample image
+	Client.Upload.photo(session, './images/apple-fruit.jpg').then(function(upload) {
+	    console.log(upload.params.uploadId);
+	    return Client.Media.configurePhoto(session, upload.params.uploadId, 'korkymckorkface');
+	}).then(function(medium) {
+	    //Log post information to console (for dev stuff)
+	    console.log(medium.params)
+	})
 })
