81f25e98714b02949540ce0b8dd8569c5546a7aa
Garlicvideos <Garlicvideos@gmail.com>
2019-02-16 17:34:51 +0800

    instagram lib test
    test of instagram lib by following instagram

--- bot/beepboop.js
+++ bot/beepboop.js
@@ -1,16 +1,18 @@
+//Include login details
+var accdetails = require('./account.json');
+
 var Client = require('instagram-private-api').V1;
-var device = new Client.Device('someuser');
-var storage = new Client.CookieFileStorage(__dirname + './cookies/someuser.json');
+var device = new Client.Device(accdetails["username"]);
+var storage = new Client.CookieFileStorage('./cookies/gitgagged.json');
  
-// And go for login
-Client.Session.create(device, storage, 'someuser', 'somepassword').then(function(session) {
-	// Now you have a session, we can follow / unfollow, anything...
-	// And we want to follow Instagram official profile
+//Login
+Client.Session.create(device, storage, accdetails["username"], accdetails["password"]).then(function(session) {
+	//Follow an account as a test
 	return [session, Client.Account.searchForUser(session, 'instagram')]   
 }).spread(function(session, account) {
 	return Client.Relationship.create(session, account.id);
 }).then(function(relationship) {
 	console.log(relationship.params)
 	// {followedBy: ... , following: ... }
-	// Yey, you just followed @instagram
+	// yayayay, you just followed @instagram
 })
