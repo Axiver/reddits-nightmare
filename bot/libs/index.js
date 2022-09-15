d0fa93dd7ff42afe880922232b8e67e715654a63
garlicvideos <garlicvideos@gmail.com>
2022-09-16 00:21:25 +0800

    Update index.js

--- bot/libs/index.js
+++ bot/libs/index.js
@@ -1,10 +1,12 @@
 //-- Import all libs --//
+const setup = require("./setup");
 const image = require("./image");
 const post = require("./post");
 const userAccount = require("./userAccount");
 
 //-- Export them as one object --//
 module.exports = {
+  ...setup,
   ...image,
   ...post,
   ...userAccount,
