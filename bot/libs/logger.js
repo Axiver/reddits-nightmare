ce27d161214d875df7f4ce3d283f2cc58aa87c54
garlicvideos <garlicvideos@gmail.com>
2022-09-21 02:48:29 +0800

    add support for colored logs
    - logs' levels are colored in the console

--- bot/libs/logger.js
+++ bot/libs/logger.js
@@ -8,8 +8,26 @@ const { capitalizeFirstLetter: capitalFirst } = require("../utils/stringUtils");
 //-- Create Winston logger --//
 //Create the logging format
 const customFormat = printf(({ level, message, label = "General", timestamp }) => {
-  return `[${timestamp}] [${label}] ${capitalFirst(level)}: ${message}`;
+  return `[${timestamp}] [${label}] ${level}: ${message}`;
 });
+
+/**
+ * Creates a logger format
+ * @param {string} loggerName The name of the logger
+ * @param {boolean} enableColor Whether or not to enable colors
+ * @returns A logger format object
+ */
+const loggerFormat = (loggerName, enableColor) => combine(
+  label({ label: loggerName }),
+  timestamp(),
+  //Make the first letter of every logger level uppercase
+  format(logLevel => {
+    logLevel.level = capitalFirst(logLevel.level);
+    return logLevel;
+  })(),
+  //Dynamically adds support for colors
+  ...(enableColor ? [format.colorize(), customFormat]: [customFormat]),
+);
 
 /**
  * Creates a new winston logger instance
@@ -19,18 +37,14 @@ function create(loggerName) {
   //Creates the logger
   const logger = createLogger({
     level: 'debug',
-    format: combine(
-      label({ label: loggerName }),
-      timestamp(),
-      customFormat
-    ),
+    format: loggerFormat(loggerName),
     transports: [
       /**
        * Logs of "Info" level and below gets printed to console
        * Logs of "Error" level and below gets written to the error log
        * Logs of "Debug" level and below gets written to the combined log
        */
-      new transports.Console({ level: "info" }),
+      new transports.Console({ level: "info", format: loggerFormat(loggerName, true)}),
       new transports.File({ filename: './logs/error.log', level: 'error' }),
       new transports.File({ filename: './logs/combined.log' }),
       /**
